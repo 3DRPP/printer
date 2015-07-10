cfc788e877fff33ded3f42cb46e783efaa01f86d
dubrzr <contact@juliendubiel.net>
2015-07-10 17:27:58 +0200

    Add HTML-CSS-JS Front-End base.

---
+++ steppers/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Dubrzr'
+
+#     File Name           :     
+#     Created By          :     Dubrzr
+#     Creation Date       :     [YYYY-MM-DD HH:MM]
+#     Last Modified       :     [2015-07-10 15:31]
+#     Description         :     
