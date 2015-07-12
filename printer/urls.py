14913f6ccdf65eed30182aaba53026840a97cf5b
dubrzr <contact@juliendubiel.net>
2015-07-13 00:19:28 +0200

    Add user administration.

--- printer/urls.py
+++ printer/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from printer.settings import SITE_URL_PREFIX
 from printer.views import home, my_login, admin, gpio_switch, tasks, add_task,\
-    my_400, my_403, my_404, my_500
+    my_400, my_403, my_404, my_500, add_user, del_user
 from django.contrib.auth.views import logout
 
 base_urlpatterns = patterns(
@@ -10,6 +10,8 @@ base_urlpatterns = patterns(
     url(r'^taches/$', tasks, name='tasks'),
     url(r'^taches/add/$', add_task, name='add_task'),
     url(r'^admin/switch_gpio/', gpio_switch, name='gpio_switch'),
+    url(r'^admin/add_user/', add_user, name='add_user'),
+    url(r'^admin/del_user/', del_user, name='del_user'),
     url(r'^admin/', admin, name='admin'),
     url(r'^connexion/$', my_login, name='login'),
     url(r'^deconnexion/$', logout, {'next_page': 'printer.views.home'}, name='logout'),
--- printer/views.py
+++ printer/views.py
@@ -6,7 +6,7 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
-from printer.models import Printable
+from printer.models import Printable, User
 from printer import gpio
 
 def home(request):
@@ -45,7 +45,10 @@ def my_login(request):
 
 @login_required
 def tasks(request):
-    waiting_tasks = Printable.objects.filter(date_start=None).order_by('-date_added').all()
+    if request.user.is_admin:
+        waiting_tasks = Printable.objects.filter(date_start=None).order_by('-date_added').all()
+    else:
+        waiting_tasks = Printable.objects.filter(date_start=None, owner=request.user).order_by('-date_added').all()
     context = {
         'waiting_tasks' : waiting_tasks
     }
@@ -100,8 +103,13 @@ def add_task(request):
     )
 
 @login_required
-def admin(request):
+def admin(request, error=None):
+    if not request.user.is_admin:
+        raise Http404()
+
     context = {
+        'error': error,
+        'users': User.objects.all(),
         'gpio_header' : gpio.header
     }
     return render(
@@ -109,6 +117,56 @@ def admin(request):
         'admin.html',
         context
     )
+
+@login_required
+def add_user(request):
+    if not request.user.is_admin:
+        raise Http404()
+
+    error = None
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('pass')
+        type = int(request.POST.get('type'))
+        if name == '' or email == '' or first_name == '' or last_name == '' or \
+            password == '' or type == '':
+            error = 'Veuillez remplir tous les champs.'
+        else:
+            try:
+                u = User()
+                u.name = name
+                u.email = email
+                u.first_name = first_name
+                u.last_name = last_name
+                u.set_password(password)
+                if type == 1:
+                    u.is_admin = True
+                u.save()
+            except:
+                error = 'Champs invalides.'
+
+    return admin(request, error)
+
+@login_required
+def del_user(request):
+    if not request.user.is_admin:
+        raise Http404()
+
+    if request.GET:
+        user_id = request.GET.get('user_id')
+        try:
+            u = User.objects.get(id=user_id)
+        except:
+            raise Http404()
+        if u == request.user:
+            raise Http404()
+        u.delete()
+
+    return admin(request)
+
 
 
 @login_required
